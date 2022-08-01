e931160e01dc8ee949f81dfe74d142d031f11d3f
Jan-Ole Koslik <janole.koslik@gmail.com>
2022-08-01 14:35:17 +0200

    Renaming

--- R/one_resample_analysis_quantile.R
+++ R/one_resample_analysis_quantile.R
@@ -1,5 +1,5 @@
 
-one_resample_analysis = function(platzhalter, y.iid, L, nscore.obj, coords, max.dist){
+one_resample_analysis = function(platzhalter, y.iid, L, nscore.obj, coords, max.dist, nbins){
   # (6) resampling from y.iid
   resmpl = sample(y.iid, size = length(y.iid), replace = T)
 
@@ -7,13 +7,13 @@ one_resample_analysis = function(platzhalter, y.iid, L, nscore.obj, coords, max.
   resmpl = L%*%resmpl
 
   # (8) backtransformation of the sample
-  resmpl = backtr(resmpl, nscore = nscore.obj, tails="none", draw=F)
+  resmpl = EgoCor:::backtr(resmpl, nscore = nscore.obj, tails="none", draw=F)
 
   # (9) repeat steps (6)-(8) <- repeating (6),(7),(8) and (10) is done by
   #                             repeating this function application
 
   # (10) semivariogram model estimation, wls
-  wls.est = sv.sep(resmpl, coords = coords, max.dist = max.dist)
+  wls.est = EgoCor:::sv.sep(resmpl, coords = coords, max.dist = max.dist, nbins = nbins)
 
   if (sum(wls.est < 0) == 0){
     return(wls.est)
