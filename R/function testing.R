e17360142a2bfefc9a3e6513e7b4762bfffb2c97
Jan-Ole Koslik <janole.koslik@gmail.com>
2022-09-13 16:57:04 +0200

    Script to test the functions

---
+++ R/function testing.R
@@ -0,0 +1,44 @@
+
+# Function testing --------------------------------------------------------
+
+library(EgoCor)
+library(EgoCorSim)
+library(BRISC)
+library(sp)
+
+head(birth)
+help(package = "EgoCor")
+
+
+# Testing with check filtering --------------------------------------------
+
+mod = vario.mod(birth, max.dist = 800, nbins = 11, shinyresults = F)
+par_uncertainty_check(mod, mod.nr = 1)
+
+# obviously works
+
+
+
+# Testing with quantile function ------------------------------------------
+
+unc = par_uncertainty_q(birth, max.dist = 800, nbins = 11)
+unc$sds
+
+# seems to work
+
+
+
+# Testing with BRISC ------------------------------------------------------
+
+help(package = "BRISC")
+View(par_uncertainty_q)
+
+coords = birth[,1:2]
+sp::coordinates(coords) = ~x+y
+coords = sp::coordinates(coords)
+
+maxdist = max(mod$variog.list[[1]]$dist)
+
+mod_brisc = BRISC_estimation(coords = birth[,1:2], y = birth$birthweight,
+                       sigma.sq = var(birth$birthweight), tau.sq = 0, phi = 1/(maxdist/3))
+View(BRISC_estimation)
