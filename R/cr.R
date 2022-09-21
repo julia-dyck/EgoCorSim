47ee3c26e49add0e022b73ac037e86cfa74be25c
Jan-Ole Koslik <janole.koslik@gmail.com>
2022-09-21 11:37:56 +0200

    Including and documenting data

---
+++ R/cr.R
@@ -0,0 +1,11 @@
+#' Complete Realization data
+#'
+#' Contains coordinates and one variable.
+#'
+#'@format A data frame with 250.000 rows and 3 variables:
+#'\describe{
+#'\item{x-coordinates}
+#'\item{y-coordinates}
+#'\item{variable}
+#'}
+"cr"
