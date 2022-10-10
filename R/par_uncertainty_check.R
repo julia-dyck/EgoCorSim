2eb7d897a570ed0c2fa4e3864d3cc0a06c268440
Jan-Ole Koslik <janole.koslik@gmail.com>
2022-10-10 09:42:38 +0200

    Introducing argument fit.method here also

--- R/par_uncertainty_check.R
+++ R/par_uncertainty_check.R
@@ -92,11 +92,11 @@
 
 par_uncertainty_check = function(vario.mod.output, mod.nr,
                            par.est = NULL, data= NULL, max.dist=NULL,nbins=NULL,
-                           B = 1000, threshold.factor = c(1.1, 1.5, 2.0, 2.5, 3.0)){
+                           B = 1000, threshold.factor = c(1.1, 1.5, 2.0, 2.5, 3.0), fit.method = NULL){
   sds = numeric(0)
   for (i in 1:length(threshold.factor)){
-    unc = EgoCor::par.uncertainty(vario.mod.output, mod.nr, par.est, data, max.dist,nbins,
-                                       B, threshold.factor[i])
+    unc = EgoCor::par.uncertainty(vario.mod.output, mod.nr, par.est, data, max.dist, nbins,
+                                       B, threshold.factor[i], fit.method)
     se = unc$se
     names(se) = c(paste0("se(nugget).", i), paste0("se(partial.sill).", i), paste0("se(shape).", i))
     sds = c(sds, se)
